aa642140a96f2d37c54bfcce428ac5cc608ed38c
Storm00212 <paulmuyalikhams@gmail.com>
2025-10-28 13:54:55 +0300

    data.ts

--- 5-usereducer/src/data/data.ts
+++ 5-usereducer/src/data/data.ts
@@ -1,5 +1,23 @@
+/**
+ * data.ts - Initial Jokes Data
+ *
+ * This file contains the initial state data for the jokes application.
+ * It demonstrates how to structure and export static data that will be
+ * used as the initial state in a useReducer hook.
+ *
+ * Key concepts:
+ * - TypeScript type imports for type safety
+ * - Static data management
+ * - Array of objects with consistent structure
+ * - Exporting data for use in other modules
+ */
+
+// Import the Joke type from the reducer file
+// This ensures our data structure matches the expected interface
 import type { Joke } from "../reducers/jokesReducer";
 
+// Export the initial jokes array
+// This will be used as the initial state in useReducer
 export const initialJokes: Joke[] = [
     {
         id: 1,
